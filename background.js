b8517f21205f2f7bf5487c6094cc7bee4089efbb
Samiyah Hoque <samiyah.hoque@stonybrook.edu>
2022-09-14 23:34:10 -0400

    Added API Key

--- background.js
+++ background.js
@@ -1,36 +1,23 @@
-function getRandomNumber(number) {
-    var max = (number + 1)
-    return Math.floor(Math.random() * Math.floor(max))
-}
-
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if(msg.name == "fetchWords"){
-            //call API
-        //const apiKey = "OUR-API-KEY"
-        //const dateStr = new Date().toISOString().slice(0,10) //2020-01-01
-        //const apiCall = 'https://api.wordnik.com/x4/words.json/wordOfTheDay?date='+dateStr+'&api_key='+apiKey
-        //wait for response
-        //send the response
+        const apiKey = "sswzcdqg4exlz7l6e2sh5ga5qvj0di0lr6nt332752bqoeyxl";
+        const dateStr = new Date().toISOString().slice(0,10); //2020-01-01
+        const apiCall = 'https://api.wordnik.com/v4/words.json/wordOfTheDay?date='+dateStr+'&api_key='+apiKey;
         
-        const wordsObj = [
-            "hello",
-            "hey",
-            "mom",
-            "dad",
-            "done"
-        ]
-        const wordsDesc = [
-            "hello d",
-            "hey d",
-            "mom d",
-            "dad d",
-            "done d"
-        ]
-
-        var number = getRandomNumber(4)
-        
-        response({word: wordsObj[number], desc: wordsDesc[number]})
-
-        //response({api: apiCall, date: dateStr})
+        //call api
+        fetch(apiCall).then(function(res) {
+            //wait for response
+            if(res.status !== 200) {
+                response({word: 'Error', desc: 'There was a problem loading the word of the day'});
+                return;
+            }
+            res.json().then(function(data) {
+                //send the response
+                response({word: data.word, desc: data.note});
+            });
+        }).catch(function(err) {
+            response({word: 'Error', desc: 'There was a problem loading the word of the day'});
+        });
     }
-})
+    return true;
+});
